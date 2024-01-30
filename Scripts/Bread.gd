1c3323502273e0987b8f5cf2b6ca0916f0909b67
CoolGrazer <marcellgarcia2009@gmai.com>
2024-01-29 20:57:23 -0500

    trying to tween bread x pos

--- Scripts/Bread.gd
+++ Scripts/Bread.gd
@@ -5,19 +5,29 @@ var velY : float = -5.0
 
 
 
+
 func _physics_process(delta):
+	position.x = snapped(position.x,1)
+	
+	
 	position.y += velY
 	velY += 0.3
 	rotation_degrees += 5
 	
-	#print(abs(position.x - 96))
-	
-	position.x = lerp(position.x,96.0,1)
-	
 	if position.y > 285:
 		position.y = 85
 		velY = -5.0
-		position.x = RandomNumberGenerator.new().randf_range(48,192 - 48)
+		position.x = RandomNumberGenerator.new().randf_range(48,48)
 	
-	#position.x = snapped(position.x,1)
+	# Repsawning this bread leads to reseting of pos
+	
 	print(position.x)
+	if !position.x == 96:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self,"position:x",96,0.5).set_ease(Tween.EASE_IN_OUT)
+	else:
+		pass
+	
+	
+	
+	#print(position.x)
