09ad8fc8e6300a00eaee688498825e60aea5c5a8
CoolGrazer <marcellgarcia2009@gmai.com>
2024-02-01 20:58:57 -0500

    Added beta audio

--- Scripts/Bread.gd
+++ Scripts/Bread.gd
@@ -10,6 +10,7 @@ var rotSpeed : float = 1.0
 var spawnBeat : int = 0
 
 func _ready():
+	$Spawn.play()
 	spawnBeat = GlobalValues.beatPos
 	x = 0
 	global_position.y = -124
@@ -22,7 +23,7 @@ func _ready():
 func _physics_process(delta):
 	
 	position.y += velY
-	velY += 0.2
+	velY += 0.22 * 2
 	rotation_degrees += 5 * rotSpeed
 	
 	position.x += velX
@@ -39,17 +40,27 @@ func _physics_process(delta):
 
 func _on_audio_stream_player_beat(beat):
 	
-	if fmod(beat,1) == 0:
-		$Kick.play()
 	
 	
 	if position == Vector2(96,116):
 		print(beat)
 	
-	if beat == spawnBeat + 2:
+	if beat == spawnBeat + 1:
+		
 		print(position)
 		velY = -5.0
 		velX = RandomNumberGenerator.new().randf_range(-5,5)
+		$Kick.play()
+		
+		spawnBeat = beat
+		x = 0
+		velX = 0
+		global_position.y = -124
+		velY = -2.5
+		rotation_degrees = randf_range(-360,360)
+		rotSpeed = randi_range(-2,2)
+		if rotSpeed == 0:
+			rotSpeed = -1
 	
 	#if fmod(beat,1) == 0:
 	#	print(position)
